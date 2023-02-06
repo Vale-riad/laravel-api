88a91601c1068a22fd72b0101a96d7c515211f12
Vale-riad <valentinariad91@gmail.com>
2023-02-06 16:37:10 +0100

    Aggiunge la create di type

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -6,6 +6,7 @@ use App\Models\Type;
 use App\Http\Requests\StoreTypeRequest;
 use App\Http\Requests\UpdateTypeRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 
 class TypeController extends Controller
 {
@@ -28,7 +29,7 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.types.create');
     }
 
     /**
@@ -39,7 +40,14 @@ class TypeController extends Controller
      */
     public function store(StoreTypeRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        $new_type = new Type();
+        $new_type->fill($data);
+        $new_type->slug = Str::slug($new_type->name);
+        $new_type->save();
+
+        return redirect()->route('admin.types.index')->with('message', "La categoria $new_type->name è stata creata con successo!");
     }
 
     /**
